05bd599185d6231877d0c852b39bb40c328af47f
Claire Irou <c.iroudayassamy@gmail.com>
2019-02-04 11:36:57 -0500

    Added accessors for String fields
    Accessors for author and title
    2.83 completed

--- Book.java
+++ Book.java
@@ -3,8 +3,8 @@
  * This might form part of a larger application such
  * as a library system, for instance.
  *
- * @author (Insert your name here.)
- * @version (Insert today's date here.)
+ * @author Claire Iroudayassamy
+ * @version Feb 4, 2019
  */
 class Book
 {
@@ -23,4 +23,21 @@ class Book
     }
 
     // Add the methods here ...
+    /**
+     * Provide access to the string holding the author's name
+     * @return author
+     */
+    public String getAuthor()
+    {
+        return author;
+    }
+    
+    /**
+     * Provide access to the string holding the book's title
+     * @return title
+     */
+    public String getTitle()
+    {
+        return title;
+    }
 }
